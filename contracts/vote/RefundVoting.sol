83fc5e6767d6d7beddd570772557fc6908c05ed7
JhChoy <choyjihuck@gmail.com>
2018-04-09 18:46:49 +0900

    add RefundVoting.sol file

---
+++ contracts/vote/RefundVoting.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.21;
+
+import "./BaseVoting.sol";
+
+contract RefundVoting is BaseVoting {
+    function RefundVoting(string _votingName) BaseVoting(_votingName) {
+
+    }
+    
+}
--- contracts/vote/VotingFactory.sol
+++ contracts/vote/VotingFactory.sol
@@ -4,22 +4,29 @@
 
 pragma solidity ^0.4.21;
 
-import "./Voting.sol";
+import "./BaseVoting.sol";
+import "./TapVoting.sol";
+import "./RefundVoting.sol";
 
 contract VotingFactory {
 
-    event CreateNewVote(address indexed vote_account, string name);
+    event CreateNewVote(address indexed vote_account, string name, string type);
     event DestroyVote(address indexed vote_account, string name);
 
     function VotingFactory(){}
-    function newVoting(string _votingName) public returns(address) {
-        Voting v = (new Voting(_votingName));
+    function newVoting(string _votingName, bool isTapVoting) public returns(address) {
+        BaseVoting v;
+        if(isTapVoting){
+            v = new TapVoting(_votingName);
+        } else{
+            v = new RefundVoting(_votingName);
+        }
         emit CreateNewVote(address(v), _votingName);
         return address(v);
     }
     function destroyVoting(address vote_account) public onlyDevelopers returns(bool){
         require(vote_account != 0x0);
-        Voting v = Voting(vote_account);
+        BaseVoting v = BaseVoting(vote_account);
         emit DestroyVote(vote_account, v.getName());
         v.destroy();
         return true;
